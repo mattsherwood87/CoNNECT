70db969e5d910942a8435475c78cdbebe9efac36
mattsherwood87 <33133019+mattsherwood87@users.noreply.github.com>
2023-04-22 19:43:10 -0400

    commits

--- docs/source/links/connect_equipment_link.py
+++ docs/source/links/connect_equipment_link.py
@@ -5,4 +5,5 @@ user_text = "equipment section of the CoNNECT website"
 url = "https://science-math.wright.edu/lab/center-of-neuroimaging-and-neuro-evaluation-of-cognitive-technologies/equipment"
 
 
+
 link.xref_links.update({link_name: (user_text, url)})
