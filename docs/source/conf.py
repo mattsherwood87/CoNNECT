4076d8cb40977be4c008507a0ad1a68c3aa4ea04
mattsherwood87 <33133019+mattsherwood87@users.noreply.github.com>
2023-05-15 14:48:09 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_adc_theme'
 html_theme_path = [sphinx_adc_theme.get_html_theme_path()]
 html_sidebars = {
-    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+    '**': ['localtoc.html', 'sourcelink.html', 'searchbox.html'],
    'using/windows': ['windowssidebar.html', 'searchbox.html'],
 }
 
