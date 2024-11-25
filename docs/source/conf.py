e504f44021ff4f116d1ee9c343e6b5b64da33953
mattsherwood87 <33133019+mattsherwood87@users.noreply.github.com>
2024-11-25 16:38:41 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ from links import *
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../support_tools'))
 
 
 # -- Project information
